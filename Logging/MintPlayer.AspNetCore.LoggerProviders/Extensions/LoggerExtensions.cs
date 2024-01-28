ca1008ae5b0a7191dcfeb13e9d0dbd59c68d92eb
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-01-28 20:53:20 +0100

    Add file logger

---
+++ Logging/MintPlayer.AspNetCore.LoggerProviders/Extensions/LoggerExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace MintPlayer.AspNetCore.LoggerProviders;
+
+public static class LoggerExtensions
+{
+    public static ILoggingBuilder AddProvider<T>(this ILoggingBuilder builder, Func<IServiceProvider, T> factory)
+        where T : class, ILoggerProvider
+    {
+        builder.Services.AddSingleton<ILoggerProvider, T>(factory);
+        return builder;
+    }
+
+    public static ILoggingBuilder AddFileProvider(this ILoggingBuilder builder)
+    {
+        return builder.AddProvider<LoggerFileProvider>((provider) =>
+        {
+            var loggerProvider = ActivatorUtilities.CreateInstance<LoggerFileProvider>(provider);
+            return loggerProvider;
+        });
+    }
+}
---
+++ Logging/MintPlayer.AspNetCore.LoggerProviders/FileLogger.cs
@@ -0,0 +1,27 @@
+﻿namespace MintPlayer.AspNetCore.LoggerProviders;
+
+internal class FileLogger : ILogger
+{
+    public FileLogger()
+    {
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return default!;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        using var fs = new FileStream("Log.txt", FileMode.Append, FileAccess.Write);
+        using var writer = new StreamWriter(fs);
+
+        writer.WriteLine(formatter(state, exception));
+        writer.WriteLine(string.Empty);
+    }
+}
