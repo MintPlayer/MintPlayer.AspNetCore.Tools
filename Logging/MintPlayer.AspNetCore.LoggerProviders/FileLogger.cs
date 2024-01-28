80d697613dee5006c917bee5198c06b8fe0e7bea
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-01-28 21:15:50 +0100

    Logger - Choose filename

--- Logging/MintPlayer.AspNetCore.LoggerProviders/FileLogger.cs
+++ Logging/MintPlayer.AspNetCore.LoggerProviders/FileLogger.cs
@@ -1,9 +1,13 @@
-﻿namespace MintPlayer.AspNetCore.LoggerProviders;
+﻿using Microsoft.Extensions.Options;
+
+namespace MintPlayer.AspNetCore.LoggerProviders;
 
 internal class FileLogger : ILogger
 {
-    public FileLogger()
+    private readonly IOptions<FileLoggerOptions> fileLoggerOptions;
+    public FileLogger(IOptions<FileLoggerOptions> fileLoggerOptions)
     {
+        this.fileLoggerOptions = fileLoggerOptions;
     }
 
     public IDisposable? BeginScope<TState>(TState state) where TState : notnull
@@ -18,10 +22,23 @@ internal class FileLogger : ILogger
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
     {
-        using var fs = new FileStream("Log.txt", FileMode.Append, FileAccess.Write);
+        var path = fileLoggerOptions?.Value?.FileName ?? "Log.txt";
+        ValidateLogFilename(path);
+
+        using var fs = new FileStream(path, FileMode.Append, FileAccess.Write);
         using var writer = new StreamWriter(fs);
 
         writer.WriteLine(formatter(state, exception));
         writer.WriteLine(string.Empty);
+    }
+
+    private void ValidateLogFilename(string filename)
+    {
+        var extension = Path.GetExtension(filename);
+        string[] allowedExts = [".txt", ".log"];
+        if (!allowedExts.Contains(extension))
+        {
+            throw new InvalidOperationException($"Logger file extension {extension} is not allowed. Only {string.Join(", ", allowedExts)} are allowed");
+        }
     }
 }
---
+++ Logging/MintPlayer.AspNetCore.LoggerProviders/FileLoggerOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace MintPlayer.AspNetCore.LoggerProviders;
+
+public class FileLoggerOptions
+{
+    public string FileName { get; set; }
+}
