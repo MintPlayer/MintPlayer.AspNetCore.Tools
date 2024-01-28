06c6a6c4a6abe1e2fb151f6d15fdc3bbe156ffdd
Pieterjan <pieterjandeclippel@msn.com>
2024-01-28 20:54:02 +0100

    Add file logger (#19)

---
+++ Logging/MintPlayer.AspNetCore.LoggerProviders/LoggerFileProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace MintPlayer.AspNetCore.LoggerProviders;
+
+internal class LoggerFileProvider : ILoggerProvider
+{
+    private readonly IServiceProvider serviceProvider;
+    public LoggerFileProvider(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        var logger = ActivatorUtilities.CreateInstance<FileLogger>(serviceProvider);
+        return logger;
+    }
+
+    public void Dispose()
+    {
+    }
+}
